2edb51ca1d30d4ac8917dfeba38ed46207ad5954
IvanGarcia17 <theivan_garcia17@hotmail.com>
2021-10-07 23:16:50 -0500

    Logueo

--- Logueo/src/main/java/mx/uv/App.java
+++ Logueo/src/main/java/mx/uv/App.java
@@ -1,4 +1,5 @@
 package mx.uv;
+import com.google.gson.*;
 
 /**
  * Hello world!
@@ -8,6 +9,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+
+        post("/saludar", (req, res) -> {
+            String l = req.queryParams("nombre");
+            String p = req.queryParams("password");
+            String respuesta;
+            System.out.println(l + " " + p);
+            
+            if (l.equals("root") && p.equals("123123"))
+            respuesta = "Bienvenido root <a href='http://127.0.0.1:5500/tabla_datos.html'>Mostrar Datos</a>";
+            else
+            respuesta = "Usuario no encontrado <a href='http://127.0.0.1:5500/envio_js_registro.html'>Registrarme</a>";
+            return respuesta;
+        });
     }
 }
